ee6a6c13ece3a384730a90214a716c9c33f03d8c
codigoreactivo <jesusjhoel.aquino@outlook.com>
2022-06-20 22:18:38 -0500

    Quima-app-Create-React-App

---
+++ fronted/src/App.js
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">QUIMA</a>
+      </header>
+      <main>
+        <h1>AQUI SE HARA UN SITIO WEB XD</h1>
+      </main>
+    </div>
+  );
+}
+
+export default App;
